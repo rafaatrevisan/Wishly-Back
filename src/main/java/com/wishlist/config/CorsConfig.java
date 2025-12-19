cefb597e58a74632df2cc2e1e7fd240ea6072403
rafaatrevisan <rafa_tre2@hotmail.com>
2025-12-19 16:59:55 -0300

    Configurações para deploy.

---
+++ src/main/java/com/wishlist/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.wishlist.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "https://wishlist.netlify.app",
+                                "http://localhost:4200"
+                        )
+                        .allowedMethods("*");
+            }
+        };
+    }
+}
